7d538aa66ac3bc1eaecc0cef0f9cd45c62a83335
pradoav <a.rozputnii@gmail.com>
2019-10-29 18:02:33 +0200

    add 1

--- lib/puts_html.rb
+++ lib/puts_html.rb
@@ -1,18 +1,16 @@
 require "puts_html/version"
 
-module PutsHtml
+
 	class PutsHtml
 	  def initialize
 	    $string
 	    $string2
 	    $string3
 	  end
-	  def p_file
+	  def self.p_file
 	    if $string.include?("<") or $string.include?(">")
 	        $string.gsub!(/[<>]/,"")
 	    end
 	    File.open('text.html', 'w'){ |f| f.write "#{$string}  <br>#{$string2} <br>#{$string3}"  }
 	  end
 	end
-	$f = PutsHtml.new
-end

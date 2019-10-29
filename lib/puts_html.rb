1a04c49655a87a17243ecf353bf1665b81ddc298
pradoav <a.rozputnii@gmail.com>
2019-10-29 20:04:18 +0200

    add 7

--- lib/puts_html.rb
+++ lib/puts_html.rb
@@ -1,17 +1,21 @@
 require "puts_html/version"
 
 class PutsHtml
-  def self.add_to_file (content, bypass_html)
+  def add_to_file (content, bypass_html)
     string = ''
     if bypass_html == false
         string = content.gsub!(/[<>]/,"")
     end
-		   f = File.new("index.html", "w")
-    f.write("<!DOCTYPE html>\n")
-    f.write("<html lang='en'>")
-    f.write("<head>\n<meta charset='utf-8'>\n<title> Web </title>\n</head>\n")
-    f.write("<body>\n#{string}\n</body>\n")
-    f.write("</html>")
-    f.close
+		File.open('index.html', 'w'){ |f| f.write 
+		"<!DOCTYPE html>\n 
+		<html lang='en'>
+			<head>\n
+				<meta charset='utf-8'>\n
+				<title> Web </title>\n
+			</head>\n
+			<body>\n
+				#{string}\n
+			</body>\n
+		</html>" }
   end
 end

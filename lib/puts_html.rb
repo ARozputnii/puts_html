f0b9ec4518b4bac426a32e91d5b07b5c8188961d
pradoav <a.rozputnii@gmail.com>
2019-10-29 19:23:21 +0200

    create gem

--- lib/puts_html.rb
+++ lib/puts_html.rb
@@ -1,17 +1,10 @@
 require "puts_html/version"
 
-module PutsHtml
-	class PutsHtml
-	  def initialize
-	    $string
-	    $string2
-	    $string3
-	  end
-	  def p_file
-	    if $string.include?("<") or $string.include?(">")
-	        $string.gsub!(/[<>]/,"")
-	    end
-	    File.open('text.html', 'w'){ |f| f.write "#{$string}  <br>#{$string2} <br>#{$string3}"  }
-	  end
+class PutsHtml
+	def p_file
+		if $string.include?("<") or $string.include?(">")
+			$string.gsub!(/[<>]/,"")
+		end
+			File.open('text.html', 'w'){ |f| f.write "#{$string}  <br>#{$string2} <br>#{$string3}"  }
 	end
 end

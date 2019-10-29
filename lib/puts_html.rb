7bcc0eec9dd69d2ac520c7ca4077c42d53692a7e
pradoav <a.rozputnii@gmail.com>
2019-10-29 17:14:36 +0200

    add lines to gem

--- lib/puts_html.rb
+++ lib/puts_html.rb
@@ -2,15 +2,16 @@ require "puts_html/version"
 
 module PutsHtml
   class Error < StandardError; end
-  class PutsFile
+	class PutsHtml
 	  def initialize
 	    $string
+	    $string2
+	    $string3
 	  end
 	  def p_file
-	  if $string.include?("<") or $string.include?(">")
-       $string.gsub!(/[<>]/,"")
-    end
-	    File.open('text.html', 'w'){ |f| f.write $string }
+	    if $string.include?("<") or $string.include?(">")
+	        $string.gsub!(/[<>]/,"")
+	    end
+	    File.open('text.html', 'w'){ |f| f.write "#{$string}  <br>#{$string2} <br>#{$string3}"  }
 	  end
 	end
-end

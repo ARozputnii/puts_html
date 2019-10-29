03985ae65dc6feeed8aa242e44d809324f401bd7
pradoav <a.rozputnii@gmail.com>
2019-10-29 17:31:09 +0200

    add new module

--- lib/puts_html.rb
+++ lib/puts_html.rb
@@ -1,7 +1,6 @@
 require "puts_html/version"
 
 module PutsHtml
-  class Error < StandardError; end
 	class PutsHtml
 	  def initialize
 	    $string
@@ -15,3 +14,4 @@ module PutsHtml
 	    File.open('text.html', 'w'){ |f| f.write "#{$string}  <br>#{$string2} <br>#{$string3}"  }
 	  end
 	end
+end
